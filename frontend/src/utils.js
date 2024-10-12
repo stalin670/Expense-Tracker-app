7827536897a2e1c6d1c79fae7d70d4340c87e680
stalin670 <amitalvin01@gmail.com>
2024-10-13 00:59:02 +0530

    setup frontend structure and dependencies

---
+++ frontend/src/utils.js
@@ -0,0 +1,15 @@
+import { toast } from 'react-toastify';
+
+export const handleSuccess = (msg) => {
+    toast.success(msg, {
+        position: 'top-right'
+    })
+}
+
+export const handleError = (msg) => {
+    toast.error(msg, {
+        position: 'top-right'
+    })
+}
+
+export const APIUrl = process.env.REACT_APP_API_URL || 'http://localhost:8080';
