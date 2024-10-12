4120b34a01605f4369459d21566e85e276614c40
stalin670 <amitalvin01@gmail.com>
2024-10-13 01:10:22 +0530

    added routing and private routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,24 @@
 import "./App.css";
 import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { useState } from "react";
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const PrivateRoute = ({ element }) => {
+    return isAuthenticated ? element : <Navigate to="/login" />;
+  };
+
   return (
     <div className="App">
-      <Signup />
+      <RefrshHandler setIsAuthenticated={setIsAuthenticated} />
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
     </div>
   );
 }
