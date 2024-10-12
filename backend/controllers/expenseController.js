000db315222d1458859750c27c5daec4ade14e6c
stalin670 <amitalvin01@gmail.com>
2024-10-12 19:37:07 +0530

    added get and delete controller for expenses

--- backend/controllers/expenseController.js
+++ backend/controllers/expenseController.js
@@ -1,7 +1,7 @@
 const User = require("../models/userModel.js");
 
 const addTransaction = async (req, res) => {
-  const { _id } = req.body;
+  const { _id } = req.user;
   try {
     const user = await User.findByIdAndUpdate(
       _id,
@@ -20,9 +20,42 @@ const addTransaction = async (req, res) => {
   }
 };
 
-const getAllTransactions = async (req, res) => {};
+const getAllTransactions = async (req, res) => {
+  const { _id } = req.user;
+  try {
+    const user = await User.findById(_id).select("expenses");
+    return res.status(200).json({
+      message: "Fetched Expenses successfully",
+      success: true,
+      data: user?.expenses,
+    });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Something went wrong", error: error, success: false });
+  }
+};
 
-const deleteTransaction = async (req, res) => {};
+const deleteTransaction = async (req, res) => {
+  const { _id } = req.user;
+  const expenseId = req.params.expenseId;
+  try {
+    const user = await User.findByIdAndUpdate(
+      _id,
+      { $pull: { expenses: { _id: expenseId } } },
+      { new: true }
+    );
+    return res.status(200).json({
+        message: "Expense deleted successfully",
+        success: true,
+        data: user?.expenses,
+      });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Something went wrong", error: error, success: false });
+  }
+};
 
 module.exports = {
   addTransaction,
