b7fcc20c765705443d452bc0616b7a0b42f8df45
stalin670 <amitalvin01@gmail.com>
2024-10-12 16:06:23 +0530

    added authController logic

---
+++ backend/controllers/authController.js
@@ -0,0 +1,69 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const User = require("../models/userModel");
+
+const signup = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (user) {
+      return res
+        .status(500)
+        .json({ message: "User already exists !", success: false });
+    }
+    const newUser = new User({ name, email, password });
+    newUser.password = await bcrypt.hash(password, 10);
+    await newUser.save();
+    return res
+      .status(201)
+      .json({ message: "Signup Successfully.", success: true });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Internal Server Error", success: false });
+  }
+};
+
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res
+        .status(404)
+        .json({ message: "User does not exist!", success: false });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res
+        .status(401)
+        .json({ message: "Password does not match", success: false });
+    }
+
+    const jwtToken = jwt.sign(
+      { email: user.email, _id: user._id },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "24h",
+      }
+    );
+
+    return res.status(200).json({
+      message: "Logged in Successfully",
+      success: true,
+      jwtToken,
+      email,
+      name: user.name,
+    });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Internal Server Error", success: false });
+  }
+};
+
+module.exports = {
+  login,
+  signup,
+};
