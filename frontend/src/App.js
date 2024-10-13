743c380be6bdfacbf2d89852fa13c01f4dbd5f09
Amit Yadav <88823371+stalin670@users.noreply.github.com>
2024-10-13 16:13:50 +0530

    added expenseForm page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,23 @@
 import "./App.css";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
-import { Navigate, Route, Routes } from "react-router-dom";
-import { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 
 function App() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-
-  const PrivateRoute = ({ element }) => {
-    return isAuthenticated ? element : <Navigate to="/login" />;
-  };
-
   return (
     <div className="App">
-      <RefrshHandler setIsAuthenticated={setIsAuthenticated} />
-      <Routes>
-        <Route path="/" element={<Navigate to="/login" />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-      </Routes>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Navigate to="/login" />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ frontend/src/pages/ExpenseForm.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import { handleError } from "../utils";
+
+const ExpenseForm = () => {
+  const [expenseInfo, setExpenseInfo] = useState({
+    amount: "",
+    text: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    const copyExpenseInfo = { ...expenseInfo };
+    copyExpenseInfo[name] = value;
+    setExpenseInfo(copyExpenseInfo);
+  };
+
+  const addExpenses = (e) => {
+    e.preventDefault();
+    const { amount, text } = expenseInfo;
+    if (!amount || !text) {
+      handleError("Please add Expense Details");
+      return;
+    }
+    addTransaction(expenseInfo);
+    setExpenseInfo({ amount: "", text: "" });
+  };
+
+  return (
+    <div className="container">
+      <h1>Expense Tracker</h1>
+      <form onSubmit={addExpenses}>
+        <div>
+          <label htmlFor="text">Expense Detail</label>
+          <input
+            onChange={handleChange}
+            type="text"
+            name="text"
+            placeholder="Enter your Expense Detail..."
+            value={expenseInfo.text}
+          />
+        </div>
+        <div>
+          <label htmlFor="amount">Amount</label>
+          <input
+            onChange={handleChange}
+            type="number"
+            name="amount"
+            placeholder="Enter your Amount..."
+            value={expenseInfo.amount}
+          />
+        </div>
+        <button type="submit">Add Expense</button>
+      </form>
+    </div>
+  );
+};
+
+export default ExpenseForm;
