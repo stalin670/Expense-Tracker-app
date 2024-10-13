cfb36c3853b482942fda906c95b0310201363c36
Amit Yadav <88823371+stalin670@users.noreply.github.com>
2024-10-13 17:18:20 +0530

    update utils

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -1,15 +1,16 @@
-import { toast } from 'react-toastify';
+import { toast } from "react-toastify";
 
 export const handleSuccess = (msg) => {
-    toast.success(msg, {
-        position: 'top-right'
-    })
-}
+  toast.success(msg, {
+    position: "top-right",
+  });
+};
 
 export const handleError = (msg) => {
-    toast.error(msg, {
-        position: 'top-right'
-    })
-}
+  toast.error(msg, {
+    position: "top-right",
+  });
+};
 
-export const APIUrl = process.env.REACT_APP_API_URL || 'http://localhost:8080';
+export const APIUrl =
+  process.env.REACT_APP_API_URL || "http://localhost:8000/api";
