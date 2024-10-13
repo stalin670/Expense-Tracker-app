5e6c70fef68ea559bd486ab3d5c08d6d9059454f
Amit Yadav <88823371+stalin670@users.noreply.github.com>
2024-10-13 16:50:09 +0530

    added : refreshHandler for quick navigation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,31 @@
 import "./App.css";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
+import Home from "./pages/Home";
 import {
   BrowserRouter as Router,
   Routes,
   Route,
   Navigate,
 } from "react-router-dom";
+import RefrshHandler from "./RefrshHandler";
+import { useState } from "react";
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const PrivateRoute = ({ element }) => {
+    return isAuthenticated ? element : <Navigate to="/login" />;
+  };
   return (
     <div className="App">
       <Router>
+        <RefrshHandler setIsAuthenticated={setIsAuthenticated} />
         <Routes>
           <Route path="/" element={<Navigate to="/login" />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
+          <Route path="/home" element={<PrivateRoute element={<Home />} />} />
         </Routes>
       </Router>
     </div>
